a987492984c524dd2ce51b359aff5cc43c9899a4
Iryna <isolodka@gmail.com>
2020-04-10 19:08:23 +0200

    done exercise 12 - ES6

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ window.addEventListener('DOMContentLoaded', function(){
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
     
-    function hideTabContent(a){
+    let hideTabContent = (a) => {
         for (let i = a; i < tabContent.length; i++){
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
-    }
+    };
 
     hideTabContent(1);
 
-    function showTabContent(b){
+    let showTabContent = (b) => {
         if (tabContent[b].classList.contains('hide')){
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
-    }
+    };
 
     info.addEventListener('click', function(event){
         let target = event.target;
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //TIMER
     let deadline = '2020-04-16';
-    function getTimeRemaining(endtime){
+    let getTimeRemaining = (endtime) => {
         let t = Date.parse(endtime) - Date.parse(new Date()),
             seconds = Math.floor((t/1000) % 60),
             minutes = Math.floor((t/1000/60) % 60),
@@ -46,9 +46,9 @@ window.addEventListener('DOMContentLoaded', function(){
             'minutes': minutes,
             'seconds': seconds
         };
-    }
+    };
 
-    function setClock(id, endtime){
+    let setClock = (id, endtime) => {
         let timer = document.getElementById(id),
             hours = timer.querySelector('.hours'),
             minutes = timer.querySelector('.minutes'),
@@ -59,21 +59,21 @@ window.addEventListener('DOMContentLoaded', function(){
             let t = getTimeRemaining(endtime);
 
             if (t.hours < 10){
-                hours.textContent = '0' + t.hours;    
+                hours.textContent =`0${t.hours}`;    
             }
             else{
                 hours.textContent = t.hours;
             }
 
             if (t.minutes < 10){
-                minutes.textContent = '0' + t.minutes;    
+                minutes.textContent = `0${t.minutes}`;    
             }
             else{
                 minutes.textContent = t.minutes;
             }
 
             if (t.seconds < 10){
-                seconds.textContent = '0' + t.seconds;    
+                seconds.textContent = `0${t.seconds}`;    
             }
             else{
                 seconds.textContent = t.seconds;
@@ -105,10 +105,8 @@ window.addEventListener('DOMContentLoaded', function(){
 
     close.addEventListener('click', function(){
         overlay.style.display = 'none';
-        document.querySelector('.more-spash').remove('more-spash');
+        document.querySelector('.more-spash').classList.remove('more-spash');
         document.body.style.overflow = "";
     });
-
-    //MORE INFORMATION
 
 });
